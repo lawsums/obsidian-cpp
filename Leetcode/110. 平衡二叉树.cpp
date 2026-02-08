9dd23baa24395d4f45410edb96c7f1c93b2bf2f7
lawsums <b22020319@njupt.edu.cn>
2026-02-08 12:16:47 +0800

    vault backup: 2026-02-08 12:16:47

--- Leetcode/110. 平衡二叉树.cpp
+++ Leetcode/110. 平衡二叉树.cpp
@@ -22,11 +22,27 @@ struct Info {
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        // 判断是否每个左子树和右子树都是平衡的
-        return handler(root).is_balanced;
-        // return isBalanced(root->left) && isBalanced(root->right);
+        return f1(root);
     }
 
+    // 复杂版
+    bool f1(TreeNode* root) {
+        // 判断是否每个左子树和右子树都是平衡的
+        return handler(root).is_balanced;
+    }
+
+    // 简洁版
+    bool f2(TreeNode* root) {
+        return b_d(root).first;
+    }
+
+private:
+    pair<bool, int> b_d(TreeNode* r){
+        if(!r) return {true,0};
+        auto [lb,ld] = b_d(r->left);
+        auto [rb,rd] = b_d(r->right);
+        return {lb && rb && abs(ld-rd)<=1, 1+max(ld,rd)};
+    }
 
     Info handler(TreeNode* root) {
         if (!root) return Info(0, true);
@@ -37,4 +53,5 @@ public:
         bool is_balanced = abs(infol.height - infor.height) <= 1 && infol.is_balanced && infor.is_balanced;
         return Info(height, is_balanced);
     }
+
 };
