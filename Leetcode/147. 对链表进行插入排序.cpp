74a11e1bd0e5e5b5b395ea0bb7bbb4e9a7d5994a
lawsums <b22020319@njupt.edu.cn>
2026-03-07 11:08:06 +0800

    vault backup: 2026-03-07 11:08:06

--- Leetcode/147. 对链表进行插入排序.cpp
+++ Leetcode/147. 对链表进行插入排序.cpp
@@ -13,8 +13,28 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode* insertionSortList(ListNode* head) {
-        vector<int> a;
-        
+    ListNode *insertionSortList(ListNode *head) {
+        // 我打算使用栈倒着排序
+        vector<ListNode *> stk;
+        auto cur = head;
+        while (cur) {
+            stk.push_back(cur);
+            cur = cur->next;
+        }
+
+        auto p1 = stk.back();
+        for (; !stk.empty(); stk.pop_back()) {
+            auto p2 = p1->next;
+            if (!p2) continue;
+            // p2 != nullptr
+            // 如果有p2就比较大小
+            while (p1->val < p2->val) {
+                if (p2->next)
+                    p2 = p2->next;
+                else
+                    break;
+            }
+            // p1->val > p2->val || p2->next == nullptr
+        }
     }
 };
