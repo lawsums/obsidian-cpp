3bbee9f14c1eb8f89b9aef50ae112d4e52acdb01
lawsums <b22020319@njupt.edu.cn>
2026-03-21 09:32:33 +0800

    vault backup: 2026-03-21 09:32:33

---
+++ Leetcode/76.最小覆盖子串 .go
@@ -0,0 +1,47 @@
+package main
+
+import "math"
+
+func minWindow(s string, t string) string {
+	cnt := map[rune]int{}
+	depts := len(t)
+	min_len := math.MaxInt
+	start_id := -1
+
+	for _, c := range t {
+		cnt[c]++
+	}
+
+	l := 0
+	for r, rc := range s {
+		if cnt[rc] > 0 {
+			depts--
+		}
+		// 减少频次, 最后会加回来所以没关系
+		cnt[rc]--
+
+		for depts == 0 {
+			// 更新答案
+			cur_len := r - l + 1
+			if cur_len < min_len {
+				cur_len = min_len
+				start_id = l
+			}
+
+			lc := s[l]
+			// 等于零说明需要加回来, <0说明是无关字符
+			if cnt[lc] == 0 {
+				depts++
+			}
+			cnt[lc]++
+			l++
+		}
+
+	}
+
+	if start_id == -1 {
+		return ""
+	} else {
+		return s[start_id : start_id+min_len]
+	}
+}
