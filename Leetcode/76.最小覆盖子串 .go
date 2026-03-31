a9924fd5fdb7b58b0197f78656f0c28622a04580
lawsums <b22020319@njupt.edu.cn>
2026-03-31 11:14:02 +0800

    vault backup: 2026-03-31 11:14:02

--- Leetcode/76.最小覆盖子串 .go
+++ Leetcode/76.最小覆盖子串 .go
@@ -24,11 +24,11 @@ func minWindow(s string, t string) string {
 			// 更新答案
 			cur_len := r - l + 1
 			if cur_len < min_len {
-                min_len = cur_len
+				min_len = cur_len
 				start_id = l
 			}
 
-            var lc rune
+			var lc rune
 			lc = s[l]
 			// 等于零说明需要加回来, <0说明是无关字符
 			if cnt[lc] == 0 {
