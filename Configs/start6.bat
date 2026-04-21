9b4cfa95c8b847810f8d172b181e3b1d7bb3a65f
lawsums <b22020319@njupt.edu.cn>
2026-04-21 13:22:43 +0800

    vault backup: 2026-04-21 13:22:43

---
+++ Configs/start6.bat
@@ -0,0 +1,20 @@
+@echo off
+chcp 65001 >nul
+echo 正在启动应用程序...
+
+start "" "F:\桌面\MyTools\贴图软件\Snipaste.exe"
+start "" "F:\桌面\MyTools\CapsLock快捷键改造.lnk"
+:: start "" "F:\桌面\MyTools\vim_ahk.lnk"
+:: start "" "F:\桌面\MyTools\vim_ahk-0.13.2\vim.ahk"
+:: start "" "F:\桌面\MyTools\vim_ahk.lnk"
+:: start "" "F:\桌面\MyTools\vim_ahk-v0.16.0\vim_ahk\vim_ahk.exe"
+start "" "G:\Code\Qt\tomato_clock2\build\tomato_clock2.exe"
+start "" "E:\Documents\AutoHotkey\VimReader3.ahk"
+start "" "F:\桌面\MyTools\鼠标改键位.lnk"
+start "" "E:\Software\Listary\Dest\Listary\Listary.exe"
+start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\inphic\inphic英云.lnk"
+start "" "E:\Documents\AutoHotkey\F8_Edge暂停.ahk"
+start "" "E:\MyPath\cherrySnippt\cherry-snippet.exe"
+
+echo 所有程序启动完成！
+timeout /t 2 >nul
