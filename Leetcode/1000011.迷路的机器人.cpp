d216be98fe711d6da89b3926210277b9b3f2d29f
lawsums <b22020319@njupt.edu.cn>
2026-04-24 19:06:27 +0800

    vault backup: 2026-04-24 19:06:26

--- Leetcode/1000011.迷路的机器人.cpp
+++ Leetcode/1000011.迷路的机器人.cpp
@@ -1,46 +1,48 @@
 #include <bits/stdc++.h>
-#include <variant>
-#include <vector>
 using namespace std;
 
 class Solution {
 private:
     vector<vector<bool>> visit;
     vector<vector<int>> path;
-    vector<vector<int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    // 题目通常只需要向右和向下，如果是四连通则保留 4 个方向
+    vector<vector<int>> dirs = {{1, 0}, {0, 1}}; 
     int n, m;
+
 public:
     vector<vector<int>> pathWithObstacles(vector<vector<int>>& obstacleGrid) {
-        n = obstacleGrid.size(), m = obstacleGrid[0].size();
+        if (obstacleGrid.empty() || obstacleGrid[0][0] == 1) return {};
+        
+        n = obstacleGrid.size();
+        m = obstacleGrid[0].size();
         visit.assign(n, vector<bool>(m, false));
-        dfs(0, 0, obstacleGrid); // r, c
-        return path;
+        
+        if (dfs(0, 0, obstacleGrid)) {
+            return path;
+        }
+        return {};
     }
 
     bool dfs(int r, int c, vector<vector<int>>& obstacleGrid) {
-        auto legal = [=](int r, int c) {
-            return (0 <= r && r <= n && 0 <= c && c <= m) && obstacleGrid[r][c] != 1;
-        };
+        // 修正边界判断：使用 < 而不是 <=
+        if (r < 0 || r >= n || c < 0 || c >= m || obstacleGrid[r][c] == 1 || visit[r][c]) {
+            return false;
+        }
 
-        // 如果已经到达了终点，直接返回True
+        visit[r][c] = true; // 标记已访问
         path.push_back({r, c});
-        visit[r][c] = true;
-        
-        if (r == n && c == m) return true;
 
-        for (int k = 0; k < 4; ++k) {
-            int nr = r + dirs[k][0], nc = c + dirs[k][1];
-            if (legal(nr, nc) && !visit[nr][nc]) {
-                if (dfs(nr, nc, obstacleGrid)) {
-                    return true;
-                }
-            }
-        } 
+        // 终点判断：坐标应为 n-1, m-1
+        if (r == n - 1 && c == m - 1) return true;
 
+        // 尝试方向
+        for (auto& dir : dirs) {
+            if (dfs(r + dir[0], c + dir[1], obstacleGrid)) return true;
+        }
+
+        // 回溯：注意！不要将 visit[r][c] 设回 false
+        // 如果这里无法到达终点，visit 保持 true 可以防止其他路径再次进入这个“死胡同”
         path.pop_back();
-        visit[r][c] = false;
-
-        // 返回False
         return false;
     }
 };
