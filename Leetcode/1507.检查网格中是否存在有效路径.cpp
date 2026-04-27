ae1800d87cdbb94c1da7b8e2cfcf5d66541da74e
lawsums <b22020319@njupt.edu.cn>
2026-04-27 10:03:54 +0800

    vault backup: 2026-04-27 10:03:54

--- Leetcode/1507.检查网格中是否存在有效路径.cpp
+++ Leetcode/1507.检查网格中是否存在有效路径.cpp
@@ -1,9 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+// 1. 设置模式开关：提交时将下面的 1 改为 0 即可
+#define DEBUG_MODE 0
+
+#if DEBUG_MODE
+    #define LOG(x) std::cout << "[DEBUG] " << x << std::endl
+    
+    // 条件打印：只有 cond 为真时才输出内容 x
+    #define CLOG(cond, x) do { if(cond) std::cout << "[COND] " << x << std::endl; } while(0)
+
+    // 多参数 LV 核心实现：支持 1-5 个参数
+    #define GET_LV(_1, _2, _3, _4, _5, NAME, ...) NAME
+    #define LV(...) std::cout << "[DEBUG] "; GET_LV(__VA_ARGS__, LV5, LV4, LV3, LV2, LV1)(__VA_ARGS__)
+
+    #define LV1(x) std::cout << #x << "=" << (x) << std::endl
+    #define LV2(x, ...) std::cout << #x << "=" << (x) << " | "; LV1(__VA_ARGS__)
+    #define LV3(x, ...) std::cout << #x << "=" << (x) << " | "; LV2(__VA_ARGS__)
+    #define LV4(x, ...) std::cout << #x << "=" << (x) << " | "; LV3(__VA_ARGS__)
+    #define LV5(x, ...) std::cout << #x << "=" << (x) << " | "; LV4(__VA_ARGS__)
+#else
+    #define LOG(x)
+    #define CLOG(cond, x)
+    #define LV(...)
+#endif
+
+// 2. 提供对常用容器的打印支持 (保持原样)
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+    os << "[";
+    for (size_t i = 0; i < v.size(); ++i) os << v[i] << (i == v.size() - 1 ? "" : ", ");
+    return os << "]";
+}
+
+template<typename K, typename V>
+std::ostream& operator<<(std::ostream& os, const std::unordered_map<K, V>& m) {
+    os << "{";
+    bool first = true;
+    for (const auto& [k, v] : m) {
+        if (!first) os << ", ";
+        os << k << ":" << v;
+        first = false;
+    }
+    return os << "}";
+}
+
 #include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 
 class UnionFind {
-private:
+public:
     vector<int> parent;  // 父节点数组
     vector<int> rank;    // 秩（树的高度）数组
 
@@ -71,7 +119,9 @@ public:
                         uf.unite(pos(i, j), pos(i, j + 1));
                     }
                 }
-            } else if (r == 2 || r == 3 || r == 4) {
+            } 
+            // 应该两个都要考虑
+            if (r == 2 || r == 3 || r == 4) {
                 if (i + 1 < n) {
                     int rd = grid[i + 1][j];
                     if (rd == 2 || rd == 5 || rd == 6) {
@@ -84,9 +134,11 @@ public:
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 tackle(i, j);
+                LV(uf.parent);
             }
         }
 
         return uf.find(0) == uf.find(pos(n - 1, m - 1));
     }
 };
+
