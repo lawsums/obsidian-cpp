0320ffc583817511f6159745b06de8e8a2c7f42c
lawsums <b22020319@njupt.edu.cn>
2026-05-23 14:20:05 +0800

    vault backup: 2026-05-23 14:20:04

--- Leetcode/1878. 检查数组是否经排序和轮转得到.cpp
+++ Leetcode/1878. 检查数组是否经排序和轮转得到.cpp
@@ -4,6 +4,20 @@ using namespace std;
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        
+        int n = nums.size();
+
+        int inc = 1;
+        int ans = 1;
+        for (int i = 1; i < 2 * n; ++i) {
+            if (nums[i % n] > nums[(i - 1) % n]) {
+                inc++; 
+                ans = max(ans, inc);
+            } else {
+                // 从自己开始重新递增
+                inc = 1;
+            }
+        }
+
+        return ans >= n;
     }
 };
