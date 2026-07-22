377346fb71f31538cb68b9504cbe6ea1193565af
lawsums <b22020319@njupt.edu.cn>
2026-07-22 10:23:34 +0800

    vault backup: 2026-07-22 10:23:33

---
+++ Leetcode/3804.操作后最大活跃区段数 II.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxActiveSectionsAfterTrade(string s, vector<vector<int>>& queries) {
+        
+    }
+};
