f001d8918e13d167381b3c37fb52de6494661caf
lawsums <b22020319@njupt.edu.cn>
2026-08-02 12:27:01 +0800

    vault backup: 2026-08-02 12:27:00

---
+++ Leetcode/3927.交替方向的最小路径代价 II.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long minCost(int m, int n, vector<vector<int>>& waitCost) {
+        
+    }
+};
---
+++ Leetcode/3948.插入一个字母的最大子序列数.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long numOfSubsequences(string s) {
+        
+    }
+};
