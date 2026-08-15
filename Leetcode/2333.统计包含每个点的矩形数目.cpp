f2dc288c138e4bf5954b3843e9fdb692dda37f26
lawsums <b22020319@njupt.edu.cn>
2026-08-15 23:45:38 +0800

    vault backup: 2026-08-15 23:45:38

---
+++ Leetcode/2333.统计包含每个点的矩形数目.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countRectangles(vector<vector<int>>& rectangles, vector<vector<int>>& points) {
+        
+    }
+};
