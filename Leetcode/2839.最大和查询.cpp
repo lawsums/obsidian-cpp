8b7df541441c579bffd6a83929b9c159195c9749
lawsums <b22020319@njupt.edu.cn>
2026-08-17 22:16:08 +0800

    vault backup: 2026-08-17 22:16:08

---
+++ Leetcode/2839.最大和查询.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maximumSumQueries(vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& queries) {
+        
+    }
+};
